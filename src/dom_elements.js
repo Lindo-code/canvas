57b01265c8137d4971fdd7a52a9f55536183b1c6
Lindo-code <sdrowvieli1@gmail.com>
2023-03-26 18:06:46 +0200

    initial

---
+++ src/dom_elements.js
@@ -0,0 +1,13 @@
+module.exports = {
+  year: document.querySelector(".year"),
+  showTitle: document.querySelector(".title"),
+  imageDiv: document.querySelector(".top-image-container"),
+  navBar: document.querySelector(".nav-bar"),
+  redLine: document.querySelector(".text-underline-long"),
+  pageTitle: document.querySelector(".curr-page-title"),
+  selfPortrait: document.querySelector(".flip-card-inner"),
+  imageLinks: document.querySelectorAll("#link"),
+  textLinks: document.querySelectorAll("#text-link"),
+  show: document.querySelectorAll(".appear"),
+  divs: document.querySelectorAll(".empty-div"), 
+};
